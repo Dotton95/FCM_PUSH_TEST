9bc36b99d81a52e6da07cea1085045819669691a
Dotton95 <dotton95@gmail.com>
2022-10-18 17:19:45 +0900

    채널 수정

--- app/src/main/java/com/example/fcm_push/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fcm_push/MyFirebaseMessagingService.kt
@@ -1,18 +1,14 @@
 package com.example.fcm_push
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import java.util.*
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     companion object {
@@ -26,11 +22,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     /* 토큰 생성 메서드 */
     override fun onNewToken(token: String) {
         Log.d(TAG,"new Token : $token")
-        sendTokenToServer(token)
+        sendTokenToServer()
 
     }
     /* 토큰값 서버에 저장하기 */
-    private fun sendTokenToServer(token:String){
+    private fun sendTokenToServer() {
         /*
         // 토큰 값을 따로 저장해둔다.
         val pref = this.getSharedPreferences("token", Context.MODE_PRIVATE)
@@ -45,12 +41,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     /* 메세지 수신 메서드 */
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        var noti_title = remoteMessage.notification!!.title.toString()
+        var noti_body = remoteMessage.notification!!.body.toString()
+
+        var data_title = remoteMessage.data["title"].toString()
+        var data_body = remoteMessage.data["body"].toString()
 
         /* 알림 빌더 */
         val notificationBuilder = NotificationCompat.Builder(this,CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)//아이콘
-            .setContentTitle(remoteMessage.data["title"].toString())//제목
-            .setContentText(remoteMessage.data["body"].toString())//내용
+            .setContentTitle(noti_title)
+            .setContentText(noti_body)
             .setAutoCancel(true) //클릭 시 자동으로 삭제되도록 설정
 
         //Oreo(26) 이상 버전에는 channel 필요
@@ -59,7 +60,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             channel.description = CHANNEL_DESCRIPTION
             notificationManager.createNotificationChannel(channel)
         }
-
         notificationManager.notify(NOTIFICATION_ID,notificationBuilder.build())
     }
 }
